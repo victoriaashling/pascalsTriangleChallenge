8ff24087707b3aa9326fbfbb3d1ea00715b12d16
victoriaashling <victoria.ashling@gmail.com>
2019-04-12 13:48:17 -0400

    adding main function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pascalsTriangleChallenge
+
 This is a small node program that will accept as an argument a number of rows and will then print out that number of rows of Pascal's Triangle.
+
+This program can be run in node -- the first argument passed in after the file name will be parsed as the number of rows of Pascal's Triangle you'd like it to print out in the console.
---
+++ victoriaAndersonPascalTriangle.js
@@ -0,0 +1,35 @@
+pascalTriangle = numberOfRows => {
+  const row1 = [1];
+  const row2 = [1, 1];
+  let laterRows = [];
+
+  if (numberOfRows > 2) {
+    for (let i = 2; i < numberOfRows; i++) {
+      let previousRow;
+
+      if (laterRows.length === 0) {
+        previousRow = row2;
+      }
+      else {
+        previousRow = laterRows[laterRows.length - 1];
+      }
+
+      let thisRow = previousRow.reduce((acc, val, index) => {
+        if (index < previousRow.length - 1) {
+          acc.push(val + previousRow[index+1]);
+        }
+        return acc;
+      }, []);
+
+      laterRows.push([1, ...thisRow, 1]);
+    }
+  }
+
+  if (numberOfRows === 1) {
+    return console.log([row1]);
+  }
+  return console.log([row1, row2, ...laterRows]);
+}
+
+let chosenNum = parseInt(process.argv[2]);
+pascalTriangle(chosenNum);
